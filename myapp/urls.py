877cf68bc6d9bca0af780711497e8ed6384e7753
Avanti-L <avantilabde8@gmail.com>
2026-02-01 19:09:56 +0530

    Add templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+# This is called namespacing: When there are more than one apps,
+# If urlpatterns in different apps have the same name, conflict will occur.
+# So to avoid it, we use namespacing as follows:
+app_name = 'myapp'
 
 urlpatterns = [
-    path('', views.index),
-    path('items/', views.item)
+    path('', views.index, name="index"),
+    path('items/', views.item),
+    # The name attribute is used for dynamic urls in html page
+    path('<int:id>/', views.detail, name="detail"), 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,8 +4,22 @@ from .models import Item
 
 # Create your views here.
 def index(request):
+    # Fetch data from the database
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    # Create context to pass data to template
+    context = {
+        'item_list': item_list
+    }
+    # Passing the context object to template
+    return render(request, "myapp/index.html", context)
+
+def detail(request, id):
+    item = Item.objects.get(id=id)
+    context = {
+        'item': item
+    }
+    return render(request, "myapp/detail.html", context)
+
 
 def item(request):
     return HttpResponse("<h1>This is an item view</h1>")
